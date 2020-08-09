ee100b27d7ecc7fe0ff51694563760b22062d2f1
Sriranjini Baskaran <ranjini_baskaran@yahoo.com>
2020-08-08 21:53:11 -0500

    Mongo db connection

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // const db = require("./models");
 
@@ -15,10 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", 
-{ useNewUrlParser: true,
-  useFindAndModify: false
- });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+
+// mongoose.connect("mongodb://localhost/workout", 
+// { useNewUrlParser: true,
+//   useFindAndModify: false
+//  });
 
  //routes
  app.use(require("./routes/api.js"));
